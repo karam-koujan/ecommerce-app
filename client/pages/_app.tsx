454d9653ab7d19c713143662100a89308923290f
karam <karamkaku2000@gmail.com>
2022-03-11 03:37:24 +0100

    add client

---
+++ client/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles/global.css";
+import "../styles/button.css";
+import type { AppProps } from 'next/app';
+
+
+function MyApp({ Component, pageProps }: AppProps){ return <Component {...pageProps} />}
+   
+
+
+export default MyApp
---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+const Home: NextPage = () => {
+  return (
+    <div>
+    <button className="btn-secondary-round">
+      shop now
+    </button>
+    </div>
+  )
+}
+
+export default Home
