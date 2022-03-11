8f50fb60d3c986237e6e2fce9c07eac767fc6879
karam <karamkaku2000@gmail.com>
2022-03-11 19:12:47 +0100

    add layout to _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/global.css";
 import "../styles/button.css";
+import "../styles/text.css";
+import "../styles/layout.css"
 import type { AppProps } from 'next/app';
+import Layout from "../components/common/layout";
 
 
-function MyApp({ Component, pageProps }: AppProps){ return <Component {...pageProps} />}
+function MyApp({ Component, pageProps }: AppProps){
+     return(
+         <Layout>
+             <Component {...pageProps} />
+         </Layout>
+      )
+}
    
 
 
