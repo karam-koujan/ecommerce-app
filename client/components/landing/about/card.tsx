e992f06fa596741a7776622a1548d3bf4d67c39f
karam <karamkaku2000@gmail.com>
2022-03-12 17:48:07 +0100

    add some modification for the small sizes devices

--- client/components/landing/about/card.tsx
+++ client/components/landing/about/card.tsx
@@ -16,7 +16,7 @@ const Card = ({title,text,image}:props)=>{
     return(
         <div  className={Styles.about__items}>
         <Image src={image.src} alt={image.alt} height={55} width={55} className={Styles.about__items__image}/>
-        <h3 className={Styles.about__items__title}>{title}</h3>
+        <h3 className={`text-center ${Styles.about__items__title}`}>{title}</h3>
         <p className={`text-center ${Styles.about__items__text}`}>
          {text}
         </p>
