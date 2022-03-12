412b1017aff72cedaafdf9e7e266a95a0825359f
karam <karamkaku2000@gmail.com>
2022-03-13 00:30:05 +0100

    add products section to the main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 import Hero from '../components/landing/hero';
 import About from '../components/landing/about';
 import Arrival from '../components/landing/arrival';
+import Products from '../components/landing/products';
 const Home: NextPage = () => {
   return (
     <>
     <Hero/>
     <About/>
     <Arrival/>
+    <Products/>
     </>
   )
 }
