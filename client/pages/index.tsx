e2ea7ddc62f6d54f8950f12c7faa0e4f062b44c8
karam <karamkaku2000@gmail.com>
2022-03-12 19:49:52 +0100

    add arrival section

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Hero from '../components/landing/hero';
 import About from '../components/landing/about';
+import Arrival from '../components/landing/arrival';
 const Home: NextPage = () => {
   return (
     <>
     <Hero/>
     <About/>
+    <Arrival/>
     </>
   )
 }
