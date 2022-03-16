b2beb8a9ff74294cf37125a3d92225f675615952
karam <karamkaku2000@gmail.com>
2022-03-16 17:43:06 +0100

    remove server directory

--- server/model/product/index.js
+++
@@ -1,7 +0,0 @@
-const productSchema = require("./product-schema");
-const productValidation = require("../validator")(productSchema);
-const buildProduct = require("./product");
-const makeProduct = buildProduct(productValidation)
-
-
-module.exports = makeProduct; 
